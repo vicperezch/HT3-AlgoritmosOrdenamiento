81acd6b319314ab822b356127091a423bc8a920a
nilsmuralles <murallesnils11@gmail.com>
2024-01-26 23:24:20 -0600

    Algoritmo Merge Sort agregado

---
+++ src/MergeSort.java
@@ -0,0 +1,62 @@
+package src;
+import java.util.Arrays;
+
+/**
+ * @author Nils Muralles
+ * @version 1.0.0
+ * @since 25/01/24
+ * Clase que contiene el algoritmo Merge Sort
+ */
+public class MergeSort<T extends Comparable<T>> implements ISort<T> {
+
+    /**
+     * @param arr Array a ordenar
+     * @return Array ordenado
+     * Utiliza el Merge Sort para ordenar un array
+     */
+    @Override
+    public T[] sort(T[] arr) {
+        int lenght = arr.length;
+        int middle = lenght/2;
+
+        if (lenght <= 1 || arr == null) {
+            return arr;
+        } 
+
+        T[] leftArr = Arrays.copyOfRange(arr, 0, middle);
+        T[] rightArr = Arrays.copyOfRange(arr, middle, lenght);
+
+        leftArr = sort(leftArr);
+        rightArr = sort(rightArr);
+        merge(leftArr, rightArr, arr);
+
+        return arr;
+    }
+
+    /**
+     * Método auxiliar que une los subarrays ordenados
+     * @param leftArr Array con primera mitad de elementos
+     * @param rightArr Array con segunda mitad de elementos
+     * @param arr Array original
+     */
+    public void merge(T[] leftArr, T[] rightArr, T[] arr) {
+        int leftIndex = 0;
+        int rightIndex = 0;
+        int originalIndex = 0;
+
+        while (leftIndex < leftArr.length && rightIndex < rightArr.length) {
+            if (leftArr[leftIndex].compareTo(rightArr[rightIndex]) < 0) {
+                arr[originalIndex++] = leftArr[leftIndex++];
+            } else {
+                arr[originalIndex++] = rightArr[rightIndex++];
+            }
+        }
+        while (leftIndex < leftArr.length) {
+            arr[originalIndex++] = leftArr[leftIndex++]; 
+        }
+        while (rightIndex < rightArr.length) {
+            arr[originalIndex++] = rightArr[rightIndex++];
+        }
+    }
+    
+}
---
+++ tests/MergeSortTest.java
@@ -0,0 +1,21 @@
+package tests;
+
+import static org.junit.Assert.assertArrayEquals;
+import org.junit.Test;
+import src.MergeSort;
+
+/**
+ * @author Nils Muralles
+ * @version 1.0.0
+ * @since 25/01/24
+ * Clase que prueba el algoritmo Merge Sort
+ */
+public class MergeSortTest {
+    @Test
+    public void testSort() {
+        MergeSort<Integer> mergeSort = new MergeSort<>();
+        Integer[] array = {5, 3, 2, 4, 1, 10, 9, 8, 7, 6, 11};
+
+        assertArrayEquals(new Integer[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}, mergeSort.sort(array));
+    }
+}
